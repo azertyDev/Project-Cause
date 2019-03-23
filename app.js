c1cb976d392a089b36724e95b5c09ed587d41f39
azertyDev <khikmatovumid@mail.ru>
2019-03-23 20:36:20 +0500

    refactored our codes[A

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
+app.get('/', (request, respond) => {
+    respond.status(200).json({
         message: 'Welcome to Project Cause',
     });
 });
 
-app.listen(port, () => {
-    console.log(`Our server listening on port ${port}!`);
-});
+app.listen(port);
